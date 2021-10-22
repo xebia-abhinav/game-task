12e110c865720331bd74c3ca05766bc96db41b5d
Abhinav Sachdeva <abhinav.sachdeva@xebia.com>
2021-10-22 16:27:03 +0530

    non-updatable constants

--- service/constants.js
+++ service/constants.js
@@ -1,7 +1,16 @@
-module.exports = {
-  status: {
-    waiting: "WAITING",
-    inProgress: "IN-PROGRESS",
-    end: "ENDED",
-  },
-};
+const constants = {};
+
+function defineConstant(key, value) {
+  Object.defineProperty(constants, key, {
+    value,
+    writable: false,
+  });
+}
+
+defineConstant("status", {
+  waiting: "WAITING",
+  inProgress: "IN-PROGRESS",
+  end: "ENDED",
+});
+
+module.exports = constants;
