cc4fd24aab10f403bd74178794fc1284600907d3
Abhinav Sachdeva <abhinav.sachdeva@xebia.com>
2021-10-22 15:40:10 +0530

    genChoice

--- service/idGenerator.js
+++ service/idGenerator.js
@@ -2,7 +2,10 @@ const genPlayerId = (n) => Date.now() + Math.floor(n * Math.random());
 
 const genGameId = () => Date.now() - 29867;
 
+const genChoice = () => (Date.now() & 1 ? -1 : 1);
+
 module.exports = {
   genGameId,
   genPlayerId,
+  genChoice,
 };
